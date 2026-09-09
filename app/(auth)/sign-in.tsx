63b8ba9824daee870b518d3b846e2837343ec4ce
Rohith G <rohithganesan208@gmail.com>
2026-09-09 18:32:41 +0530

    Updated code initial development

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>SignIn</Text>
+      <Link href="/(auth)/sign-in" className="mt-4 text-lg text-primary">
+        Go to SignIn
+      </Link>
+    </View>
+  )
+}
+
+export default SignIn

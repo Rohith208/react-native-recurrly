c764e449b8f1cfa80f249b9cf29130ac533d54a5
Rohith G <rohithganesan208@gmail.com>
2026-09-18 16:47:07 +0530

    Implement Fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 import {styled} from "nativewind";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
  
@@ -8,30 +8,35 @@ const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 p-5 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
+    <SafeAreaView className="flex-1 p-5 bg-background">
+      <Text className="text-7xl font-sans-extrabold">
+        Home
       </Text>
-      <Link href="/onboarding" className="mt-4 text-lg text-primary">
+      {/* <Text className="text-7xl font-bold">
+        Home
+      </Text> */}
+      <Link href="/onboarding" className="mt-4 p-4 font-sans-bold rounded text-white text-lg bg-primary">
         Go to Onboarding
       </Link>
 
-      <Link href="/(auth)/sign-in" className="mt-4 text-lg text-primary">
+      <Link href="/(auth)/sign-in" className="mt-4 p-4 font-sans-bold rounded text-white text-lg bg-primary">
         Go to Sign In
       </Link>
 
-      <Link href="/(auth)/sign-up" className="mt-4 text-lg text-primary">
+      <Link href="/(auth)/sign-up" className="mt-4 p-4 font-sans-bold rounded text-white text-lg bg-primary">
         Go to Sign Up
       </Link>
 
-      <Link href="/subscriptions/spotify" className="mt-4 text-lg text-primary">
+      {/* <Link href="/subscriptions/spotify" className="mt-4 text-lg text-primary">
         Spotify
       </Link>
 
       <Link href={{
         pathname: "/subscriptions/[id]",
         params: { id: "claude" },
-      }}></Link>
+      }}>
+        claude max subscription
+      </Link> */}
     </SafeAreaView>
   );
 }
--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,6 +1,4 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { Text } from 'react-native'
 import {styled} from "nativewind";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
  
